23799d0186d2e936c24d953026ae641f2f0d03a0
eeti286 <eerik.linjama@gmail.com>
2022-10-10 23:26:07 +0300

    uudet tehtävät

---
+++ Rajapinnat/APIproject/Form1.Designer.cs
@@ -0,0 +1,229 @@
+﻿
+namespace APIproject
+{
+    partial class NHLAPIProject
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.GridPanel = new System.Windows.Forms.Panel();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.TeamName = new System.Windows.Forms.Label();
+            this.TeamDescription = new System.Windows.Forms.Label();
+            this.MatchInfoLabel = new System.Windows.Forms.Label();
+            this.NextMatch = new System.Windows.Forms.Label();
+            this.Venue = new System.Windows.Forms.Label();
+            this.VenueInfo = new System.Windows.Forms.Label();
+            this.MatchDate = new System.Windows.Forms.Label();
+            this.TeamLogo = new System.Windows.Forms.PictureBox();
+            this.roster = new System.Windows.Forms.ListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.TeamLogo)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // GridPanel
+            // 
+            this.GridPanel.AutoScroll = true;
+            this.GridPanel.Location = new System.Drawing.Point(12, 44);
+            this.GridPanel.Name = "GridPanel";
+            this.GridPanel.Size = new System.Drawing.Size(466, 697);
+            this.GridPanel.TabIndex = 5;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Consolas", 10F, System.Drawing.FontStyle.Bold);
+            this.label2.Location = new System.Drawing.Point(182, 21);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(90, 20);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "W | T | L";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Consolas", 10F, System.Drawing.FontStyle.Bold);
+            this.label3.Location = new System.Drawing.Point(14, 21);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(54, 20);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Teams";
+            // 
+            // TeamName
+            // 
+            this.TeamName.AutoSize = true;
+            this.TeamName.Font = new System.Drawing.Font("Consolas", 15F, System.Drawing.FontStyle.Bold);
+            this.TeamName.Location = new System.Drawing.Point(480, 46);
+            this.TeamName.Name = "TeamName";
+            this.TeamName.Size = new System.Drawing.Size(0, 29);
+            this.TeamName.TabIndex = 8;
+            this.TeamName.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // TeamDescription
+            // 
+            this.TeamDescription.AutoSize = true;
+            this.TeamDescription.Font = new System.Drawing.Font("Consolas", 10F, System.Drawing.FontStyle.Italic);
+            this.TeamDescription.Location = new System.Drawing.Point(483, 78);
+            this.TeamDescription.Name = "TeamDescription";
+            this.TeamDescription.Size = new System.Drawing.Size(0, 20);
+            this.TeamDescription.TabIndex = 9;
+            this.TeamDescription.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // MatchInfoLabel
+            // 
+            this.MatchInfoLabel.AutoSize = true;
+            this.MatchInfoLabel.Font = new System.Drawing.Font("Consolas", 12F, System.Drawing.FontStyle.Bold);
+            this.MatchInfoLabel.Location = new System.Drawing.Point(480, 156);
+            this.MatchInfoLabel.Name = "MatchInfoLabel";
+            this.MatchInfoLabel.Size = new System.Drawing.Size(131, 23);
+            this.MatchInfoLabel.TabIndex = 10;
+            this.MatchInfoLabel.Text = "Next match:";
+            this.MatchInfoLabel.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // NextMatch
+            // 
+            this.NextMatch.AutoSize = true;
+            this.NextMatch.Font = new System.Drawing.Font("Consolas", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.NextMatch.Location = new System.Drawing.Point(482, 187);
+            this.NextMatch.Name = "NextMatch";
+            this.NextMatch.Size = new System.Drawing.Size(0, 20);
+            this.NextMatch.TabIndex = 11;
+            this.NextMatch.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // Venue
+            // 
+            this.Venue.AutoSize = true;
+            this.Venue.Font = new System.Drawing.Font("Consolas", 10F);
+            this.Venue.Location = new System.Drawing.Point(481, 275);
+            this.Venue.Name = "Venue";
+            this.Venue.Size = new System.Drawing.Size(0, 20);
+            this.Venue.TabIndex = 13;
+            this.Venue.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // VenueInfo
+            // 
+            this.VenueInfo.AutoSize = true;
+            this.VenueInfo.Font = new System.Drawing.Font("Consolas", 12F, System.Drawing.FontStyle.Bold);
+            this.VenueInfo.Location = new System.Drawing.Point(479, 244);
+            this.VenueInfo.Name = "VenueInfo";
+            this.VenueInfo.Size = new System.Drawing.Size(76, 23);
+            this.VenueInfo.TabIndex = 12;
+            this.VenueInfo.Text = "Venue:";
+            this.VenueInfo.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // MatchDate
+            // 
+            this.MatchDate.AutoSize = true;
+            this.MatchDate.Font = new System.Drawing.Font("Consolas", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.MatchDate.Location = new System.Drawing.Point(482, 207);
+            this.MatchDate.Name = "MatchDate";
+            this.MatchDate.Size = new System.Drawing.Size(0, 20);
+            this.MatchDate.TabIndex = 14;
+            this.MatchDate.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // TeamLogo
+            // 
+            this.TeamLogo.BackColor = System.Drawing.SystemColors.Control;
+            this.TeamLogo.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.TeamLogo.Cursor = System.Windows.Forms.Cursors.No;
+            this.TeamLogo.Location = new System.Drawing.Point(863, 12);
+            this.TeamLogo.Name = "TeamLogo";
+            this.TeamLogo.Size = new System.Drawing.Size(100, 100);
+            this.TeamLogo.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.TeamLogo.TabIndex = 15;
+            this.TeamLogo.TabStop = false;
+            // 
+            // roster
+            // 
+            this.roster.AccessibleName = "roster";
+            this.roster.BackColor = System.Drawing.SystemColors.Menu;
+            this.roster.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.roster.Font = new System.Drawing.Font("Consolas", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.roster.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.roster.FormattingEnabled = true;
+            this.roster.ItemHeight = 20;
+            this.roster.Location = new System.Drawing.Point(483, 361);
+            this.roster.Name = "roster";
+            this.roster.Size = new System.Drawing.Size(480, 380);
+            this.roster.TabIndex = 17;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Consolas", 12F, System.Drawing.FontStyle.Bold);
+            this.label1.Location = new System.Drawing.Point(480, 327);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(87, 23);
+            this.label1.TabIndex = 18;
+            this.label1.Text = "Roster:";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // NHLAPIProject
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(975, 753);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.roster);
+            this.Controls.Add(this.TeamLogo);
+            this.Controls.Add(this.MatchDate);
+            this.Controls.Add(this.Venue);
+            this.Controls.Add(this.VenueInfo);
+            this.Controls.Add(this.NextMatch);
+            this.Controls.Add(this.MatchInfoLabel);
+            this.Controls.Add(this.TeamDescription);
+            this.Controls.Add(this.TeamName);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.GridPanel);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Name = "NHLAPIProject";
+            this.Text = "NHL";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.TeamLogo)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Panel GridPanel;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label TeamName;
+        private System.Windows.Forms.Label TeamDescription;
+        private System.Windows.Forms.Label MatchInfoLabel;
+        private System.Windows.Forms.Label NextMatch;
+        private System.Windows.Forms.Label Venue;
+        private System.Windows.Forms.Label VenueInfo;
+        private System.Windows.Forms.Label MatchDate;
+        private System.Windows.Forms.PictureBox TeamLogo;
+        private System.Windows.Forms.ListBox roster;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Rajapinnat/APIproject/NHLTeam/NHLTeam.cs
@@ -0,0 +1,279 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace APIproject.NHLTeam
+{
+
+    // Root myDeserializedClass = JsonConvert.DeserializeObject<Root>(myJsonResponse);
+    public class Away
+    {
+        public LeagueRecord leagueRecord { get; set; }
+        public int score { get; set; }
+        public Team team { get; set; }
+    }
+
+    public class Broadcast
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+        public string type { get; set; }
+        public string site { get; set; }
+        public string language { get; set; }
+    }
+
+    public class Conference
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+        public string link { get; set; }
+    }
+
+    public class Content
+    {
+        public string link { get; set; }
+    }
+
+    public class Date
+    {
+        public string date { get; set; }
+        public int totalItems { get; set; }
+        public int totalEvents { get; set; }
+        public int totalGames { get; set; }
+        public int totalMatches { get; set; }
+        public List<Game> games { get; set; }
+        public List<object> events { get; set; }
+        public List<object> matches { get; set; }
+    }
+
+    public class Division
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+        public string nameShort { get; set; }
+        public string link { get; set; }
+        public string abbreviation { get; set; }
+    }
+
+    public class Franchise
+    {
+        public int franchiseId { get; set; }
+        public string teamName { get; set; }
+        public string link { get; set; }
+    }
+
+    public class Game
+    {
+        public int gamePk { get; set; }
+        public string link { get; set; }
+        public string gameType { get; set; }
+        public string season { get; set; }
+        public DateTime gameDate { get; set; }
+        public Status status { get; set; }
+        public Teams teams { get; set; }
+        public Venue venue { get; set; }
+        public List<Broadcast> broadcasts { get; set; }
+        public List<RadioBroadcast> radioBroadcasts { get; set; }
+        public Content content { get; set; }
+        public MetaData metadata { get; set; }
+    }
+
+    public class Teams
+    {
+        public Away away { get; set; }
+        public Home home { get; set; }
+    }
+
+    public class GameType
+    {
+        public string id { get; set; }
+        public string description { get; set; }
+        public bool postseason { get; set; }
+    }
+
+    public class Home
+    {
+        public LeagueRecord leagueRecord { get; set; }
+        public int score { get; set; }
+        public Team team { get; set; }
+    }
+
+    public class LeagueRecord
+    {
+        public int wins { get; set; }
+        public int losses { get; set; }
+        public int ot { get; set; }
+        public string type { get; set; }
+    }
+
+    public class MetaData
+    {
+        public string timeStamp { get; set; }
+    }
+
+    public class Metadata2
+    {
+        public bool isManuallyScored { get; set; }
+        public bool isSplitSquad { get; set; }
+    }
+
+    public class NextGameSchedule
+    {
+        public int totalItems { get; set; }
+        public int totalEvents { get; set; }
+        public int totalGames { get; set; }
+        public int totalMatches { get; set; }
+        public MetaData metaData { get; set; }
+        public List<Date> dates { get; set; }
+    }
+
+    public class Person
+    {
+        public int id { get; set; }
+        public string fullName { get; set; }
+        public string link { get; set; }
+    }
+
+    public class Position
+    {
+        public string code { get; set; }
+        public string name { get; set; }
+        public string type { get; set; }
+        public string abbreviation { get; set; }
+    }
+
+    public class RadioBroadcast
+    {
+        public string name { get; set; }
+        public string type { get; set; }
+    }
+
+    public class Root
+    {
+        public string copyright { get; set; }
+        public List<Team> teams { get; set; }
+    }
+
+    public class Roster
+    {
+        public List<Roster> roster { get; set; }
+        public string link { get; set; }
+        public Person person { get; set; }
+        public string jerseyNumber { get; set; }
+        public Position position { get; set; }
+    }
+
+    public class Split
+    {
+        public Stat stat { get; set; }
+        public Team team { get; set; }
+    }
+
+    public class Stat
+    {
+        public int gamesPlayed { get; set; }
+        public object wins { get; set; }
+        public object losses { get; set; }
+        public object ot { get; set; }
+        public object pts { get; set; }
+        public string ptPctg { get; set; }
+        public object goalsPerGame { get; set; }
+        public object goalsAgainstPerGame { get; set; }
+        public object evGGARatio { get; set; }
+        public string powerPlayPercentage { get; set; }
+        public object powerPlayGoals { get; set; }
+        public object powerPlayGoalsAgainst { get; set; }
+        public object powerPlayOpportunities { get; set; }
+        public string penaltyKillPercentage { get; set; }
+        public object shotsPerGame { get; set; }
+        public object shotsAllowed { get; set; }
+        public object winScoreFirst { get; set; }
+        public object winOppScoreFirst { get; set; }
+        public object winLeadFirstPer { get; set; }
+        public object winLeadSecondPer { get; set; }
+        public object winOutshootOpp { get; set; }
+        public object winOutshotByOpp { get; set; }
+        public object faceOffsTaken { get; set; }
+        public object faceOffsWon { get; set; }
+        public object faceOffsLost { get; set; }
+        public string faceOffWinPercentage { get; set; }
+        public double shootingPctg { get; set; }
+        public double savePctg { get; set; }
+        public string penaltyKillOpportunities { get; set; }
+        public string savePctRank { get; set; }
+        public string shootingPctRank { get; set; }
+    }
+
+    public class Status
+    {
+        public string abstractGameState { get; set; }
+        public string codedGameState { get; set; }
+        public string detailedState { get; set; }
+        public string statusCode { get; set; }
+        public bool startTimeTBD { get; set; }
+    }
+
+    public class Team
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+        public string link { get; set; }
+        public Venue venue { get; set; }
+        public string abbreviation { get; set; }
+        public string teamName { get; set; }
+        public string locationName { get; set; }
+        public string firstYearOfPlay { get; set; }
+        public Division division { get; set; }
+        public Conference conference { get; set; }
+        public Franchise franchise { get; set; }
+        public List<TeamStat> teamStats { get; set; }
+        public Roster roster { get; set; }
+        public NextGameSchedule nextGameSchedule { get; set; }
+        public string shortName { get; set; }
+        public string officialSiteUrl { get; set; }
+        public int franchiseId { get; set; }
+        public bool active { get; set; }
+        public Away away { get; set; }
+        public Home home { get; set; }
+    }
+
+    public class Team2
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+        public string link { get; set; }
+    }
+
+    public class TeamStat
+    {
+        public Type type { get; set; }
+        public List<Split> splits { get; set; }
+    }
+
+    public class TimeZone
+    {
+        public string id { get; set; }
+        public int offset { get; set; }
+        public string tz { get; set; }
+    }
+
+    public class Type
+    {
+        public string displayName { get; set; }
+        public GameType gameType { get; set; }
+    }
+
+    public class Venue
+    {
+        public string name { get; set; }
+        public string link { get; set; }
+        public string city { get; set; }
+        public TimeZone timeZone { get; set; }
+        public int id { get; set; }
+    }
+
+
+
+}
